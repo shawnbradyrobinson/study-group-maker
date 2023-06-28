4248198c8f6670e632365101c874d5f21bf5fdd4
shawnbradyrobinson <shawnbradyrobinson1@gmail.com>
2023-06-27 20:31:14 -0500

    getting the groups route working the way Kendall needs

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
-
-const { User } = require("../../models");
+const fs = require('fs');
+const { Users } = require("../../models");
 
 router.get('/', (req, res) => {
     console.log("Get from the api/users");
@@ -11,7 +11,7 @@ router.get('/', (req, res) => {
     //This code is currently taken directly from Week 14 example 19 Ins-Middleware, so if there's something that doesn't fit, that's probably why...
    
     try {
-      const dbUserData = await User.create({
+      const dbUserData = await Users.create({
         //id? id: req.body.id,
         user_name: req.body.user_name,
         first_name: req.body.first_name,
@@ -30,6 +30,12 @@ router.get('/', (req, res) => {
       res.status(500).json(err);
     }
   });
+
+  router.get('/data', (req, res) => {
+
+    //findByPK or findAll 
+    res.send(sequelize.models.User);
+  })
 
   
 
