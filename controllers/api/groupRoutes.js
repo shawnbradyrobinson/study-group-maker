fa273cc128f4b8ef134ba4fc50392aed834685c0
kendall529 <kendall.smith529@gmail.com>
2023-06-29 20:50:09 -0500

    updated from github

--- controllers/api/groupRoutes.js
+++ controllers/api/groupRoutes.js
@@ -10,14 +10,7 @@ console.log("api/groups message");
 res.send("hi");
 next()
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-})
-=======
-}).post('/', (req, res) => {
-=======
 }).post('/', async (req, res) => {
->>>>>>> fea7bc5c8b64d8061376cd458839edda39424e00
     //Create a new group
     //This code is currently taken directly from Week 14 example 19 Ins-Middleware, so if there's something that doesn't fit, that's probably why...
     try {
@@ -46,22 +39,6 @@ next()
 
 
 
-<<<<<<< HEAD
-
-
-
-
-});
-
-
-
-
-
-
->>>>>>> 082b07b291cd276eded6bebd3525309310466c36
-
-=======
->>>>>>> fea7bc5c8b64d8061376cd458839edda39424e00
 router.get('/data', (req, res, next) => {
     console.log("hi there");
     const data = fs.readFileSync("./seeds/json_data/groups_data.json", "utf8");
@@ -69,49 +46,7 @@ router.get('/data', (req, res, next) => {
     res.json(data);
     
     }).post('/data', (req, res, next) => {
-<<<<<<< HEAD
-        const {group_name, study_topic, skill_level, zoom_link, meet_time} = req.body; 
-
-        if (group_name && study_topic && skill_level && zoom_link && meet_time){
-
-            const newGroup = {
-                group_name, 
-                study_topic, 
-                skill_level, 
-                zoom_link, 
-                meet_time
-            };
-
-            fs.readFile("./seeds/json_data/groups_data.json", "utf8", (err, data) =>{
-                if (err){
-                    console.error(err);
-                }else{
-                    const parsedGroups = JSON.parse(data);
-
-                    parsedGroups.push(newGroup);
-
-                    const stringifiedGroups = JSON.stringify(parsedGroups);
-
-                    fs.writeFile("./seeds/json_data/groups_data.json", stringifiedGroups, (err) => err ? console.log(err) : console.log("successfully wrote new group to groups_data.json"));
-
-                }
-
-
-            });
-
-            res.json(newGroup);
-            next()
-
-
-
-        }else{
-            console.log("data isn't coming through yet");
-            next()
-        }
-    })
-=======
     });
->>>>>>> 082b07b291cd276eded6bebd3525309310466c36
 //JSON.parse(data) will be used to 
 
 module.exports = router; 
