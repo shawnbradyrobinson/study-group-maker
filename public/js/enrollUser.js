51dc05bbcd6e829aff937f74fac25398931b5572
namhn123 <namhn1231@gmail.com>
2023-07-10 02:46:09 -0500

    fixed add-user issues

--- public/js/enrollUser.js
+++ public/js/enrollUser.js
@@ -1,3 +1,23 @@
+// //drop down lists all users and enrolls the user into the group
+// document.querySelector("#user-group-list").addEventListener("c ", async function enrollUserHandler(event) {
+//   event.preventDefault();
+//   var element = event.target;
+//   if (event.target.matches(".add-user-button")) {//checks if element clicked on has add-user class
+//     alert("bye");
+//         var study_group_id = element.id.substring(9);//removes text from id to get id number
+//         var user_id = element.querySelector(".user_names").value;//gets the form value
+//         if (study_group_id && user_id) {
+//             const response = await fetch(`/api/enrollments/${user_id}`, {//post fetch to enrollment api
+//               method: 'POST',
+//               body: JSON.stringify({ study_group_id }),
+//               headers: { 'Content-Type': 'application/json' },
+//             });
+//             if (!response.ok) {
+//               alert(response.statusText);
+//             }
+//         }
+//     }  
+//   });
 document.querySelector("#user-group-list").addEventListener("submit", async function enrollUserHandler(event) {
   event.preventDefault();
 
@@ -12,10 +32,11 @@ document.querySelector("#user-group-list").addEventListener("submit", async func
               body: JSON.stringify({ study_group_id }),
               headers: { 'Content-Type': 'application/json' },
             });
-            if (!response.ok) {
+            if (response.ok) {
+              document.location.replace(`/groups/${study_group_id}`);
+            } else {
               alert(response.statusText);
             }
         }
     }  
   });
-
