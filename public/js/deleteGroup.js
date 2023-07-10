f7831c36b8adafd608b7a44e6c9312837c67445c
namhn123 <namhn1231@gmail.com>
2023-07-09 21:44:32 -0500

    added bio

--- public/js/deleteGroup.js
+++ public/js/deleteGroup.js
@@ -1,12 +1,7 @@
+//queryselector that finds if the element has a deletebutton class and 
 document.querySelector("#user-group-list").addEventListener("click", async function deleteGroupHandler(event) {
-    alert("hi1")
     if(event.target.matches('.deleteButton')) {
-      alert("hi");
       const id = event.target.getAttribute('data-id');
-      // console.log("--------------------------");
-      // console.log("We are inside of the event target has attribute code block right now");
-      // console.log("--------------------------");
-      // console.log(`event.target value is ${id}`);
       const response = await fetch(`/api/groups/${id}`, {
         method: 'DELETE',
         headers: { 'Content-Type': 'application/json' },
@@ -15,15 +10,5 @@ document.querySelector("#user-group-list").addEventListener("click", async funct
       }).catch(err => {
         alert(err);
       });
-  
-    //   if(response.ok) {
-    //     console.log("Deleting the group...");
-    //     document.location.replace('/profile');
-  
-    //   } else {
-  
-    //     alert('Could not delete project.')
-    //   }
-    // }
     }
   });
